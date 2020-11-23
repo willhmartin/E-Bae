aa52fcb0e8fd104c95952edd3b293a4fe3a11253
Will Martin <whmartinwork@outlook.com>
2020-11-23 18:34:29 +0800

    added model and controller for user, created api/v1 directories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  post '/login', to: 'login#login'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
+  
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [ :show, :create, :update, :destroy ]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_23_094524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
+    t.string "gender"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "open_id"
+  end
 
 end
