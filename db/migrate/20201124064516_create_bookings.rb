59c100e06efe2943f6bb8a91a80be78f25887819
Arana Wang <aranayww@gmail.com>
2020-11-24 15:00:20 +0800

    create a booking model.

---
+++ db/migrate/20201124064516_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :date
+      t.string :location
+      t.references :user, null: false, foreign_key: true
+      t.integer :total
+      t.references :service, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
