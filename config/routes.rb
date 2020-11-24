d94c80faa1eb2832921a663b0a7fcfe5518187b2
Will Martin <whmartinwork@outlook.com>
2020-11-24 13:34:13 +0800

    added Services model and controller. Created seed for users and services

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   post '/login', to: 'login#login'
-  get 'users/show'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [ :show, :create, :update, :destroy ]
+      resources :services, only: [ :index, :show, :create, :update, :destroy ]
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Service.destroy_all
+
+user1 = User.create(role: "user", name: "Magoolio", gender: "male", age: 42)
+user2 = User.create(role: "user", name: "Bob", gender: "male", age: 60)
+user3 = User.create(role: "user", name: "Erica", gender: "female", age: 25)
+user4 = User.create(role: "user", name: "Arana", gender: "female", age: 25)
+user5 = User.create(role: "user", name: "Kyle", gender: "male", age: 36)
+user6 = User.create(role: "user", name: "Will", gender: "male", age: 27)
+
+Service.create!(area: "Shanghai", duration: 3, price: 400, content: "I'll cover up the cracks in your life", user: user1 )
+Service.create!(area: "Chengdu", duration: 3, price: 400, content: "I'll cover up the cracks in your life", user: user2)
+Service.create!(area: "Shanghai", duration: 3, price: 400, content: "I'll cover up the cracks in your life", user: user3)
+Service.create!(area: "Beijing", duration: 3, price: 400, content: "I'll cover up the cracks in your life", user: user4)
+Service.create!(area: "Xian", duration: 3, price: 400, content: "I'll cover up the cracks in your life", user: user5)
